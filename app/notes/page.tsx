2441e9d245fe3e680ffa5c7736e3b4bcc086737a
Collin O'Conner <croconner522@gmail.com>
2025-12-29 14:53:34 -0500

    Fixed build error

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,33 @@
 import { createClient } from '@/lib/supabase/server'
+import { Suspense } from 'react'
 
-export default async function Page() {
+async function NotesList() {
   const supabase = await createClient()
   const { data: notes } = await supabase.from('notes').select()
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+  if (!notes) {
+    return <p>No notes found</p>
+  }
+
+  return (
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          {note.title}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default function Page() {
+  return (
+    <main>
+        <h2>Notes:</h2>
+        {/* We wrap the data component in Suspense */}
+        <Suspense fallback={<p>Loading notes...</p>}>
+            <NotesList />
+        </Suspense>
+    </main>
+  );
 }
